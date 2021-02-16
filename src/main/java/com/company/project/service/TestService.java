680b969c99b9627ddd3e12249e85630d46c38ecd
kotarou <zk990127@gmail.com>
2021-02-16 16:05:08 +0800

    init

---
+++ src/main/java/com/company/project/service/TestService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.Test;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2021/02/15.
+ */
+public interface TestService extends Service<Test> {
+
+}
